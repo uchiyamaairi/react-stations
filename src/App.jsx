7bd573825ff643794a8d4733d543cf7ee9767dc2
uchiyamaairi <nkc20236174@st.denpa.jp>
2023-11-17 13:42:03 +0900

    station2できた

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,5 @@ import './App.css'
  * @type {() => JSX.Element}
  */
 export const App = () => {
-  return (
-    <div>
-      <h2>Hello, world!</h2>
-    </div>
-  )
+  return <header>Dogアプリ</header>
 }
