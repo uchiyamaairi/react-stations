0e9b972d9939f2976d15bb24adaedacdc45cd8ee
uchiyamaairi <nkc20236174@st.denpa.jp>
2024-01-10 13:06:44 +0900

    変更?

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 // DO NOT DELETE
 
 import './App.css'
-
+import { dog } from 'https://images.dog.ceo/breeds/corgi-cardigan/n02113186_6539.jpg'
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
-  return <header>Dogアプリ</header>
+  return (
+    <div>
+      <header>
+        <p>Dogアプリ</p>
+      </header>
+      <p>犬の画像を表示するサイトです</p>
+      <img src={dog} alt="inu" />
+    </div>
+  )
 }
